3c6d1ee0e7129a4586cf668df000fb511f0af019
vladsolntsev <vlad.solnt@gmail.com>
2020-10-28 14:55:15 +0100

    testfun

--- src/Model/LanguageManager.php
+++ src/Model/LanguageManager.php
@@ -16,7 +16,7 @@ class LanguageManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
-    
+
     public function selectCategories(): array
     {
         return $this->pdo->query('SELECT identifier FROM ' . $this->table)->fetchAll();
