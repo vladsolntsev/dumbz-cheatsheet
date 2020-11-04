bbeb79bc77ed8c72ad1346edc92da1a6f73f4d2b
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-11-04 15:18:15 +0100

    Create new post in Database DONE

--- src/Model/PostManager.php
+++ src/Model/PostManager.php
@@ -33,7 +33,7 @@ class PostManager extends AbstractManager
 
     public function selectAllMyFavorites($user): array
     {
-       return $this->pdo->query('SELECT *, (post.like - post. dislike) as popularity FROM ' . $this->table . ' LEFT JOIN favorite ON post.id = favorite.post_id WHERE favorite.user_id=' . $user . ';')->fetchAll();
+        return $this->pdo->query('SELECT *, (post.like - post. dislike) as popularity FROM ' . $this->table . ' LEFT JOIN favorite ON post.id = favorite.post_id WHERE favorite.user_id=' . $user . ';')->fetchAll();
 
     }
 
@@ -42,4 +42,24 @@ class PostManager extends AbstractManager
         return $this->pdo->query('SELECT *, (post.like - post. dislike) as popularity FROM ' . $this->table . ' WHERE post.user_id=' . $user . ';')->fetchAll();
     }
 
+    public function createPost($user, $title, $content, $language): void
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (user_id, title, content, language_id, creation_at) VALUES (:user, :title, :content, :language, now())");
+            $statement->bindValue('user', $user, \PDO::PARAM_INT);
+            $statement->bindValue('title', $title, \PDO::PARAM_STR);
+            $statement->bindValue('content', $content, \PDO::PARAM_STR);
+            $statement->bindValue('language', $language, \PDO::PARAM_INT);
+            $statement->execute();
+    }
+
+public function test($name, $email, $password, $admin): array
+   {
+       $statement = $this->pdo->prepare("INSERT INTO user (name, email, password, admin) VALUES (:name, :email, :password, :admin)");
+           $statement->bindValue('user', $name, \PDO::PARAM_INT);
+           $statement->bindValue('content', $email, \PDO::PARAM_STR);
+           $statement->bindValue('content', $password, \PDO::PARAM_STR);
+           $statement->bindValue('language', $admin, \PDO::PARAM_BOOL);
+           $statement->execute();
+           return (int)$this->pdo->lastInsertId();
+   }
 }
