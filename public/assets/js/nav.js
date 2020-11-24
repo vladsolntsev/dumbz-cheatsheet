978aabf376162beecada16235bc7699db6de7d51
Blustery33 <guillaume.moultson@gmail.com>
2020-11-24 12:15:05 +0100

    Css in progress, add new nav bar

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,13 +1,3 @@
-/*  JS Navigation */
-const burger = document.getElementById('burger');
-const menuBurger = document.getElementById('menu-burger')
-burger.addEventListener('click', event => {
-    menuBurger.classList.toggle('sidebar-nav')
-    burger.classList.toggle('burger-menu-white')
-    document.getElementById('menu-burger').classList.toggle('display-block-nav');
-})
-
-/* End JS Navigation */
 
 /* JS Favorites */
 const stars = document.getElementsByClassName('fa-star');
