9831ab9a25e5c37b0945816ff7ccdd0accee8b7f
vladsolntsev <vlad.solnt@gmail.com>
2020-11-25 11:44:48 +0100

    colors posts category ok

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -15,11 +15,12 @@ class LanguageController extends AbstractController
         $languageById = new LanguageManager();
         $allPostsOrderedByDate = $languageById->selectPostsByLanguageOrderedBy($id, 'creation_at');
         $allPostsOrderedByPopularity = $languageById->selectPostsByLanguageOrderedBy($id, 'popularity');
-
+        $colors = ['#EE908A','#EEAE8A', '#EEDC8A', '#B5E1EE', '#D7B0EE' ];
         return $this->twig->render('Home/index.html.twig', [
             'languages' => $categories,
             'all_posts_by_date' => $allPostsOrderedByDate,
-            'all_posts_by_pop' => $allPostsOrderedByPopularity
+            'all_posts_by_pop' => $allPostsOrderedByPopularity,
+            'colors' => $colors,
         ]);
     }
 }
